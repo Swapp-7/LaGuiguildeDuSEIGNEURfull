29b870a5ba5348455ae0a5fdbbc08247a0398024
yodup <yoenie.dupuy@etu.univ-smb.fr>
2026-06-02 09:50:58 +0200

    FRONT Correction du nom de la route et ajout de paramètre reqHP

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -29,16 +29,17 @@ final class CharacterController extends AbstractController
         ]);
     }
 
-    #[Route('/most-hp', name: 'app_character_most_hp', methods: ['GET'])]
-    public function mostHp(CharacterRepository $characterRepository): Response
+    #[Route('/life/{reqHP}', name: 'app_character_most_hp', methods: ['GET'])]
+    public function mostHp(CharacterRepository $characterRepository, int $reqHP): Response
     {
         $characters = array_filter(
             $characterRepository->findAll(),
-            fn(Character $c) => $c->getLife() > 100
+            fn(Character $c) => $c->getLife() > $reqHP
         );
 
         return $this->render('character/most_hp.html.twig', [
             'characters' => $characters,
+            'reqHP' => $reqHP,
         ]);
     }
 
